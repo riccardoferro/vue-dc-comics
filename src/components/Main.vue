834fcc1e1f299327245b9bf5ca9f7ee4b329dbd0
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-06 16:25:10 +0200

    update main component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,9 @@
 //HTML
 <template>
     <main>
-
+      <div class="container70">
+          <span>  --&gt; Content goes here &lt;-- </span>
+      </div>
     </main>
 </template>
 
@@ -14,5 +16,15 @@
 
 //CSS
 <style scoped>
+
+
+    main {
+        height: 100px;
+        font-weight: 1000;
+        line-height: 100px;
+        vertical-align: middle;
+        color: white;
+        background-color: black;
+    }
 
 </style>
