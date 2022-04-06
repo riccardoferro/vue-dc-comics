431bfdb37c49cf729a8e7519c7b3056bbe742c58
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-06 15:45:20 +0200

    init component footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+//HTML
+<template>
+    <footer>
+
+    </footer>
+</template>
+
+//JS
+<script>
+  export default {
+      name: "ComponentFooter"
+  }
+</script>
+
+//CSS
+<style scoped>
+
+</style>
