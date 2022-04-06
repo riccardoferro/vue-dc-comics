29f079ab6db2cf7060a41be76cae37ebdb2ac2b3
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-06 21:48:22 +0200

    completed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,12 +2,56 @@
 <template>
     <footer>
         <div class="container70 d-flex">
-            <div>
-
+            <div class="left-side-footer d-flex wrap">
+                <div>
+                    <span>DC COMICS</span>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div>
+                    <span>DC COMICS</span>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                        <li>News</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div>
+                    <span>DC COMICS</span>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                    </ul>
+                </div>
+                <div>
+                    <span>DC COMICS</span>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                    </ul>
+                </div>
             </div>
+            <!-- logo footer -->
             <div class="image">
             </div>
-            <!-- <img src="../assets/img/dc-logo-bg.png" alt=""> -->
         </div>
     </footer>
 </template>
@@ -29,13 +73,20 @@
       height: 300px;
   }
 
+  .left-side-footer {
+    padding-top: 30px;
+    width: 40%;
+  }
+
   .d-flex {
     display: flex;
     justify-content: space-between;
-    align-items: center;
   }
-/* '../assets/img/dc-logo-bg.png' */
  
+ .wrap {
+   flex-wrap: wrap;
+ }
+
  .image {
   width:400px;
   height:300px;
@@ -45,7 +96,21 @@
   background-position: center;
  }
 
-  /* img {
-    height: 400px;
-  } */
+div {
+  color: white;
+  font-weight: 700;
+  font-size: 15px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 10px 0;
+}
+
+li {
+  color: grey;
+  font-size: 10px;
+  padding: 4px 0;
+}
+
 </style>
