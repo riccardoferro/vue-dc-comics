429f3a82e72a51061c4c65d0387bb1086bac3453
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-06 15:40:26 +0200

    init Component Header

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+//HTML
+<template>
+    <header>
+    </header>
+</template>
+
+//JS
+<script>
+  export default {
+      name: "ComponentHeader",
+  }
+</script>
+
+// CSS
+<style scoped>
+
+</style>
