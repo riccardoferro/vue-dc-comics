a0519d090497b226159ac047428179a56cf49769
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-06 15:44:09 +0200

    init component Main

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
+//HTML MAIN
 <template>
     <div id="app">
         <Header/>
+        <Main/>
     </div>
 </template>
 
+//JS MAIN
 <script>
 import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Main
   }
 }
 </script>
 
+//CSS MAIN
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+//HTML
+<template>
+    <main>
+
+    </main>
+</template>
+
+//JS
+<script>
+  export default {
+      name: "ComponentMain"
+  }
+</script>
+
+//CSS
+<style scoped>
+
+</style>
