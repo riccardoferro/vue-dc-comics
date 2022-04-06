c1c078c240e1b73553ab46b771d79b6e4691939b
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-06 16:17:52 +0200

    completed header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,26 @@
-//HTML
+// HTML
 <template>
     <header>
+        <div class="container70 d-flex">
+            <!-- //logo -->
+            <img src="../assets/img/dc-logo.png" alt="logo_dc_comics">
+
+            <!-- navbar -->
+            <div>
+                <ul class="d-flex">
+                  <li>CHARACTERS</li>
+                  <li>COMICS</li>
+                  <li>MOVIES</li>
+                  <li>TV</li>
+                  <li>GAMES</li>
+                  <li>COLLECTIBLES</li>
+                  <li>VIDEOS</li>
+                  <li>FANS</li>
+                  <li>NEWS</li>
+                  <li>SHOP</li>
+                </ul>
+            </div>
+        </div>
     </header>
 </template>
 
@@ -13,5 +33,42 @@
 
 // CSS
 <style scoped>
+
+/* Header custom */
+header {
+  min-height: 80px;
+  border-top: 25px solid black;
+  padding: 15px 0;
+}
+
+/* Header custom logo*/
+header img {
+  max-width: 65px;
+}
+
+/* Display flex  */
+.d-flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+/* Custom unordered list */
+ul {
+  list-style-type: none;
+}
+
+ul li {
+  margin: 0 10px;
+  font-size: 10px;
+  height: 65px;
+  line-height: 65px;
+  vertical-align: center;
+}
+
+ul li:hover {
+  color: blue;
+  border-bottom: 5px solid blue;
+}
 
 </style>
