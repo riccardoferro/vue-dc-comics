311a3c81abd0b48900ab635a8cd1191bb0ed3eaf
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-07 17:33:15 +0200

    insert two text box

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,10 @@
 //HTML
 <template>
     <main>
-        <div class="container70 d-flex">
+        <span class="title"> 
+            CURRENT SERIES
+        </span>
+        <div class="container70 d-flex-cards">
             <!-- we gonna create a single comic for each element in the data dc-comics
                 so we gonna create an other sub-component and send him the single comic (object)
              -->
@@ -9,6 +12,9 @@
             <!-- we need the key to identify uniquely the the single comic book -->
             <SingleComic v-for="(comic,index) in comics" :key="'comic'+index" :fumetto="comic"/>
             
+        </div>
+        <div class="container70 d-flex-center pb-30">
+            <span class="text-box ">LOAD MORE</span>
         </div>
     </main>
 </template>
@@ -45,13 +51,38 @@ main {
     color: white;
     background-color: hsl(0deg 0% 11%);
     padding-top: 30px;
+    position: relative;
 }
 
-.d-flex {
+.d-flex-cards {
     padding: 20px 0;
     display: flex;
     flex-wrap: wrap;
 }
 
+.d-flex-center {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.pb-30 {
+    padding-bottom: 30px;
+}
+
+.text-box {
+    background-color: hsl(209deg 98% 49%);
+    padding: 10px 70px;
+    font-size: 12px;
+}
+
+.title {
+    background-color: hsl(209deg 98% 49%);
+    position: absolute;
+    top: 0;
+    left: 225px;
+    transform: translateY(-50%);
+    padding: 10px 15px;
+}
 
 </style>
