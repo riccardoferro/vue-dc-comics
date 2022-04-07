73d6f734baa5cb594a6ae8344e4c2ee6768caf5c
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-07 16:54:12 +0200

    Inserting image and text of a comic book and custom this last one

--- src/components/subcomponents/SingleComic.vue
+++ src/components/subcomponents/SingleComic.vue
@@ -1,13 +1,44 @@
 <template>
-  
+  <div class="card">
+      <img :src="fumetto.thumb" alt="img of the comic book">
+      <div>{{fumetto.series}}</div>
+  </div>
 </template>
 
 <script>
 export default {
+    name: "Single_Comic",
 
+
+    /*This is the single comic
+       {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+            "price": "$19.99",
+            "series": "Action Comics",
+            "type": "comic book"
+       },
+    */
+
+    props: {
+         //here we receive the single comic that have type object
+        fumetto: Object
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+  .card {
+    width: calc(100% / 6);
+    color: white;
+    font-weight: normal;
+    font-size: smaller;
+    text-transform: uppercase;
+    padding-bottom: 50px;
+
+    img {
+      width: 75%;
+    }
+}
 
 </style>
